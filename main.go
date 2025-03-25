afc19ed3970f1f3c903a4881e271d51928698fe5
Ben <bcebulski96@gmail.com>
2025-03-25 07:14:42 -0400

    Zone info only requires apiToken Setup the client to send the requests Makes sure to include the context in the function Add messages and panics Make sure to close the response body (use defer so it's done at the end of the function) Read the response body into a new var using io.ReadAll which will read the response body until EOF Cast the body var into a string to get the JSON string

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"time"
@@ -17,27 +18,46 @@ const AUTH_HEADER_KEY = "Authorization"
 func main() {
 	apiToken := os.Args[1]
 	apiEmail := os.Args[2]
-	client := &http.Client{}
-
-	GetZoneInformation(*client, apiToken, apiEmail)
-}
-
-func GetZoneInformation(client http.Client, apiToken string, apiEmail string) {
+	fmt.Println(apiEmail)
+	// Create a context which enables a 5s timeout
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+
+	GetZoneInformation(*client, ctx, apiToken)
+}
+
+// Method to get Zone Information
+func GetZoneInformation(client http.Client, ctx context.Context, apiToken string) {
+	// Create the request setting the context, the method, the endpoint, and the body
+	// GET requests don't have a body so pass nil
 	req, err := http.NewRequestWithContext(ctx, "GET", ZONES_ENDPOINT, nil)
-	if err != nil {
+
+	if err != nil { // Handle any errors relating to creating the request
+		fmt.Println("Error creating the request") // Print a message
 		panic(err)
 	}
+	// Add the Authorization header to the request using the API Token
 	req.Header.Set(AUTH_HEADER_KEY, MakeAuthHeaderValue(apiToken))
-
-	resp, err := client.Do(req)
+	resp, err := client.Do(req) // Fire the request
 
 	if err != nil {
+		fmt.Println("Error firing request")
 		panic(err)
 	}
 
-	fmt.Println(resp)
+	defer resp.Body.Close()            // Make sure to close the response Body
+	body, err := io.ReadAll(resp.Body) // Easier way to read the response body rather than manually managing a new byte[]
+	if err != nil {
+		fmt.Println("Error reading response body")
+		panic(err)
+	}
+	//Cast the byte[] as a string to read the body JSON normally
+	fmt.Println(string(body))
 }
 
 func MakeAuthHeaderValue(apiToken string) string {
